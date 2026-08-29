3f7086ded91cd00fa8d59847b9162c937f27a7ca
daniel-skopek <daniel.skopek1@gmail.com>
2026-08-29 04:28:28 +0200

    Fixed fake entity generation (26.2 support)

--- src/main/java/su/nightexpress/excellentcrates/hologram/entity/FakeEntity.java
+++ src/main/java/su/nightexpress/excellentcrates/hologram/entity/FakeEntity.java
@@ -2,9 +2,11 @@ package su.nightexpress.excellentcrates.hologram.entity;
 
 import org.bukkit.Location;
 import org.jetbrains.annotations.NotNull;
-import su.nightexpress.nightcore.util.EntityUtil;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class FakeEntity {
+
+    private static final AtomicInteger ID_GENERATOR = new AtomicInteger(Integer.MAX_VALUE);
 
     private final int      id;
     private final Location location;
@@ -16,7 +18,7 @@ public class FakeEntity {
 
     @NotNull
     public static FakeEntity create(@NotNull Location location) {
-        return new FakeEntity(EntityUtil.nextEntityId(), location);
+        return new FakeEntity(ID_GENERATOR.decrementAndGet(), location);
     }
 
     public int getId() {
